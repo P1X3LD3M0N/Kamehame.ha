5f50ae1aae43bd8fbdaf51e0e8a9c523890ff9fa
P1X3LD3M0N <77390209+P1X3LD3M0N@users.noreply.github.com>
2023-03-11 09:32:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,21 +24,18 @@ document.addEventListener('keydown',function(e)
 document.addEventListener('touchstart',function(e)
 {
     console.log(e)
-    if (e.key == 'k' || e.key == 'K')
-    {
-        document.getElementById("png").style.display = 'none'
-        document.getElementById("gif").style.display = 'inline'
-        document.getElementById("head").innerHTML = ""
-        setTimeout(function(){
-            audio.play()
-        },5000)
-        setTimeout(function(){
-            document.getElementById("gif").style.display = 'none'
-            document.getElementById("png").style.display = 'inline'
-            img = document.getElementById("gif")
-            imgUrl = img.src
-            img.src = "#"
-            img.src = imgUrl
-        },13000)
-    }
+    document.getElementById("png").style.display = 'none'
+    document.getElementById("gif").style.display = 'inline'
+    document.getElementById("head").innerHTML = ""
+    setTimeout(function(){
+        audio.play()
+    },5000)
+    setTimeout(function(){
+        document.getElementById("gif").style.display = 'none'
+        document.getElementById("png").style.display = 'inline'
+        img = document.getElementById("gif")
+        imgUrl = img.src
+        img.src = "#"
+        img.src = imgUrl
+    },13000)
 })
